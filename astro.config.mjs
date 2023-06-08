896530a89ffbff041e2e369c442d417f31bc5e57
zahra hakimi <dev.zahrahakimi777@gmail.com>
2023-06-08 02:09:30 +0200

    add github folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://github.com/zahrahkm.github.io',
+  base: '/zahrahakimi.github.io',
 });
+
 
